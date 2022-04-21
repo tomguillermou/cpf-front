89f7ac40e9a33c6827af1480fb1c9480b9797435
tomguillermou <tomguillermou@gmail.com>
2022-04-22 00:01:18 +0200

    🔧 Setup API URL for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import { Environment } from './environment.interface';
 
 export const environment: Environment = {
     production: true,
-    apiUrl: '',
+    apiUrl: 'https://api.easy-form.net',
 };
