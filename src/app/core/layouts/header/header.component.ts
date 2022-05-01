ced8214dc5a1a57915fe2e3e27495870199e3b0f
tomguillermou <tomguillermou@gmail.com>
2022-05-01 12:06:01 +0200

    💄 Only show dashboard in header for admin

--- src/app/core/layouts/header/header.component.ts
+++ src/app/core/layouts/header/header.component.ts
@@ -2,21 +2,24 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { AuthenticationService } from 'app/core/services';
+import { User } from 'app/shared/models';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
 })
 export class HeaderComponent {
-    public showHeader = false;
+    public isUserConnected: boolean;
+    public isUserConnectedAnAdmin: boolean;
 
     constructor(private _router: Router, private _authService: AuthenticationService) {
         this._authService.getUserConnected().subscribe((user) => {
-            this.showHeader = Boolean(user);
+            this.isUserConnected = Boolean(user);
+            this.isUserConnectedAnAdmin = this.isUserConnected && user.isAdmin;
         });
     }
 
-    onLogout(): void {
+    public onLogout(): void {
         this._authService.logout();
         this._router.navigateByUrl('/login');
     }
